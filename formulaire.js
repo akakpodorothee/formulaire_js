7673ed100fee71fccc1cca0b3662c3055aeb1f3d
iMac G1 <imacg1@iMac-de-Energy-24.local>
2022-05-06 14:32:08 +0000

    dorothee js commit

---
+++ formulaire.js
@@ -0,0 +1,21 @@
+function myfunction() {
+let Nom = document.querySelector("#Nom");
+let nom = document.querySelector("#Noms");
+nom.innerHTML = Nom.value;
+
+let Reseau = document.querySelector("#Reseau");
+let reseau = document.querySelector("#email");
+reseau.innerHTML = Reseau.value;
+
+let chiffre = document.querySelector("#chiffre");
+let chiffres = document.querySelector("#chiffres");
+chiffres.innerHTML = chiffre.value;
+
+let coach = document.querySelector("#Coachs");
+let Coach = document.querySelector("#Coach");
+Coach.innerHTML = coach.value;
+
+let Text = document.querySelector("#Text");
+let text = document.querySelector("#text");
+text.innerHTML = Text.value;
+ }
