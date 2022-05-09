6ea5cc35d4bb548d6f8c55b47ef88baaa2b94d6d
iMac G1 <imacg1@iMac-de-Energy-24.local>
2022-05-09 09:02:53 +0000

    ajouter-formulaire-js-corriger

--- formulaire.js
+++ formulaire.js
@@ -1,21 +1,23 @@
 function myfunction() {
+// ================= Nom=====================================
 let Nom = document.querySelector("#Nom");
 let nom = document.querySelector("#Noms");
 nom.innerHTML = Nom.value;
-
+//================== Address Email============================= 
 let Reseau = document.querySelector("#Reseau");
 let reseau = document.querySelector("#email");
 reseau.innerHTML = Reseau.value;
-
+// ================= Satisfaction au cours====================
 let chiffre = document.querySelector("#chiffre");
 let chiffres = document.querySelector("#chiffres");
 chiffres.innerHTML = chiffre.value;
-
+// ================== Choix de Coach=========================
 let coach = document.querySelector("#Coachs");
 let Coach = document.querySelector("#Coach");
 Coach.innerHTML = coach.value;
-
+// ================== Description ===========================
 let Text = document.querySelector("#Text");
 let text = document.querySelector("#text");
 text.innerHTML = Text.value;
  }
+ 
